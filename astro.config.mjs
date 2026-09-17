b589484c9ce1fc6d1b4d69fb4528ae0e2291ded2
tienvybui05 <tienvy27052005@gmail.com>
2026-09-17 14:56:34 +0700

    Cai dat Sitemap de tu dong SEO Google

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,11 @@
 import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
+import sitemap from "@astrojs/sitemap";
 
 export default defineConfig({
-  site: "https://webprostudio.vn",
+  site: "https://bluvy.io.vn",
   output: "static",
+  integrations: [sitemap()],
   vite: {
     plugins: [tailwindcss()],
   },
