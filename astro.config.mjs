065c2bfa4653f5c5bca706c24bf349cfc4bfd441
tienvybui05 <tienvy27052005@gmail.com>
2026-09-17 12:43:41 +0700

    Go bo Vercel adapter khong can thiet cho web static

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,9 @@
 import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
-import vercel from '@astrojs/vercel';
 
 export default defineConfig({
   site: "https://webprostudio.vn",
   output: "static",
-  adapter: vercel(),
   vite: {
     plugins: [tailwindcss()],
   },
